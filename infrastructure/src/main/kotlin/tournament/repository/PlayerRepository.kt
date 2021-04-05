bf3ba7244e86f1163b5e51365cd2438af1c3832d
Drenard <denis.renard@pole-emploi.fr>
2021-04-05 23:46:01 +0200

    implements persistence side

--- infrastructure/src/main/kotlin/tournament/repository/PlayerRepository.kt
+++ infrastructure/src/main/kotlin/tournament/repository/PlayerRepository.kt
@@ -1,7 +1,7 @@
 package tournament.repository
 
 import tournament.entities.Player
-data class PlayerRepository(internal val pseudo: String) {
+data class PlayerRepository(internal val pseudo: String, var points: Int = 0) {
     internal fun toPlayer(): Player {
         return Player(this.pseudo)
     }
