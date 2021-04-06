cd830f4caeb9e2c5f5cbff1dd3838869518adc3a
Drenard <denis.renard@pole-emploi.fr>
2021-04-06 23:07:21 +0200

    refacto PlayerFeaturesTest.kt

--- domain/src/test/kotlin/tournament/PlayerFeaturesTest.kt
+++ domain/src/test/kotlin/tournament/PlayerFeaturesTest.kt
@@ -1,9 +1,12 @@
 package tournament
 
+import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import tournament.entities.Player
 import tournament.ports.spi.PlayerPersistencePort
@@ -13,97 +16,112 @@ class PlayerFeaturesTest {
     private val repository = mockk<PlayerPersistencePort>()
     private val playerFeatures = PlayerFeatures(repository)
 
-    @Test
-    fun `create a player`() {
-        // Given
-        val playerPseudo = "toto"
-        val playerExpected = Player(playerPseudo, 0)
-        every { repository.save(any()) }.returns(playerExpected)
+    @BeforeEach
+    fun init() {
+        clearMocks(repository)
+    }
 
-        // When
-        playerFeatures.`add new player`(playerPseudo)
+    @Nested
+    inner class CreatePlayer {
+        @Test
+        fun `create a player`() {
+            // Given
+            val playerPseudo = "toto"
+            val playerExpected = Player(playerPseudo, 0)
+            every { repository.save(any()) }.returns(playerExpected)
 
-        // Then
-        verify {
-            repository.save(withArg {
-                assertThat(it).usingRecursiveComparison().isEqualTo(playerExpected)
-            })
+            // When
+            playerFeatures.`add new player`(playerPseudo)
+
+            // Then
+            verify {
+                repository.save(withArg {
+                    assertThat(it).usingRecursiveComparison().isEqualTo(playerExpected)
+                })
+            }
+        }
+
+        @Test
+        fun `should return player created when admin add a player `() {
+            // Given
+            val playerPseudo = "toto"
+            val playerExpected = Player(playerPseudo, 0)
+
+            every { repository.save(any()) }.returns(playerExpected)
+
+            // When
+            val playerCreated = playerFeatures.`add new player`(playerPseudo)
+
+            // Then
+            assertThat(playerCreated).usingRecursiveComparison().isEqualTo(playerExpected)
+        }
+
+        @Test
+        fun `should called just once the repository when admin create a player`() {
+            // Given
+            val playerPseudo = "toto"
+            val playerExpected = Player(playerPseudo, 0)
+
+            every { repository.save(any()) }.returns(playerExpected)
+
+            // When
+            playerFeatures.`add new player`(playerPseudo)
+
+            // Then
+            verify(exactly = 1) {
+                repository.save(any())
+            }
         }
     }
 
-    @Test
-    fun `should return player created when admin add a player `() {
-        // Given
-        val playerPseudo = "toto"
-        val playerExpected = Player(playerPseudo, 0)
+    @Nested
+    inner class GetAllPlayers {
+        @Test
+        fun `should return all players sorted by points from repository`() {
+            // Given
+            val firstPlayerPseudo = "toto"
+            val secondPlayerPseudo = "tata"
+            val firstPlayer = Player(firstPlayerPseudo, 0)
+            val secondPlayer = Player(secondPlayerPseudo, 10)
+            every { repository.getAll() }.returns(listOf(firstPlayer, secondPlayer))
 
-        every { repository.save(any()) }.returns(playerExpected)
+            // When
+            val listPlayers = playerFeatures.`retrieve all players sorted by points`()
 
-        // When
-        val playerCreated = playerFeatures.`add new player`(playerPseudo)
-
-        // Then
-        assertThat(playerCreated).usingRecursiveComparison().isEqualTo(playerExpected)
-    }
-
-    @Test
-    fun `should called just once the repository when admin create a player`() {
-        // Given
-        val playerPseudo = "toto"
-        val playerExpected = Player(playerPseudo, 0)
-
-        every { repository.save(any()) }.returns(playerExpected)
-
-        // When
-        playerFeatures.`add new player`(playerPseudo)
-
-        // Then
-        verify(exactly = 1) {
-            repository.save(any())
+            //Then
+            assertThat(listPlayers.first()).usingRecursiveComparison().isEqualTo(secondPlayer)
+            assertThat(listPlayers.component2()).usingRecursiveComparison().isEqualTo(firstPlayer)
         }
     }
 
-    @Test
-    fun `should return all players sorted by points from repository`() {
-        // Given
-        val firstPlayerPseudo = "toto"
-        val secondPlayerPseudo = "tata"
-        val firstPlayer = Player(firstPlayerPseudo, 0)
-        val secondPlayer = Player(secondPlayerPseudo, 10)
-        every { repository.getAll() }.returns(listOf(firstPlayer, secondPlayer))
+    @Nested
+    inner class UpdatePlayerPoints {
+        @Test
+        fun `should return true when player points are modify`() {
+            //Given
+            val player = Player("toto")
+            every { repository.updatePoints(player.pseudo, player.points) }.returns(true)
 
-        // When
-        val listPlayers = playerFeatures.`retrieve all players sorted by points`()
+            // When
+            val success = playerFeatures.`update points player`(player.pseudo, player.points)
 
-        //Then
-        assertThat(listPlayers.first()).usingRecursiveComparison().isEqualTo(secondPlayer)
-        assertThat(listPlayers.component2()).usingRecursiveComparison().isEqualTo(firstPlayer)
+            // Then
+            assertThat(success).isTrue
+        }
+
+        @Test
+        fun `should return false when player points are modify and player not exists`() {
+            //Given
+            val player = Player("toto")
+            every { repository.updatePoints(player.pseudo, player.points) }.returns(false)
+
+            // When
+            val success = playerFeatures.`update points player`(player.pseudo, player.points)
+
+            // Then
+            assertThat(success).isFalse
+        }
     }
 
-    @Test
-    fun `should return true when player points are modify`(){
-        //Given
-        val player = Player("toto")
-        every { repository.updatePoints(player.pseudo, player.points) }.returns(true)
-
-        // When
-        val success = playerFeatures.`update points player`(player.pseudo, player.points)
-
-        // Then
-        assertThat(success).isTrue
-    }
-
-    @Test
-    fun `should return false when player points are modify and player not exists`(){
-        //Given
-        val player = Player("toto")
-        every { repository.updatePoints(player.pseudo, player.points) }.returns(false)
-
-        // When
-        val success = playerFeatures.`update points player`(player.pseudo, player.points)
-
-        // Then
-        assertThat(success).isFalse
-    }
 }
 
